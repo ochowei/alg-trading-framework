fa482f37c14c32016c7ef22be67ee01e4240eb8d
William <82436358+ochowei@users.noreply.github.com>
2025-11-03 21:31:06 +0800

    update filepath

--- src/sj_trading/config.py
+++ src/sj_trading/config.py
@@ -10,7 +10,7 @@ class Config:
 
     # 常量
     PERFORMANCE_TARGET = "sharpe"
-    YFINANCE_FILE_NAME = "backup/stock_price.csv"
+    YFINANCE_FILE_NAME = "data/stock_price.csv"
 
     # 監控清單
     WATCH_TARGETS = []
